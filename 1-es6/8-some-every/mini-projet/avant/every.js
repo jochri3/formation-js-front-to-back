1d0503f790f9168c0ef91cd602a057a2a8bdb243
Christian Lisangola <jochri3@gmail.com>
2021-11-10 10:26:25 +0300

    some-every

---
+++ 1-es6/8-some-every/mini-projet/avant/every.js
@@ -0,0 +1,3 @@
+// Mettre en place une validation basique du formulaire où tous les champs doivent être remplis
+//Sinon, un message d'erreur en rouge apparait en disant que certains champs ne sont pas remplis
+//Et si tous les champs sont remplis correctement,un message en vert disant félicitation devra apparaitre 
