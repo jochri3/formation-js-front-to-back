3f575777cac0151c59b83e4e3c7c502284ce7eee
Christian Lisangola <jochri3@gmail.com>
2021-11-12 07:40:57 +0300

    destructuration

---
+++ 1-es6/4-destructuration/mini-projet/avant/app.js
@@ -0,0 +1,6 @@
+// L'exercice consiste à effectuer un appel Ajax pour recevoir les données
+//depuis l'url suivant : http://jsonplaceholder.typicode.com/users
+//Pendant que la requête sera entrain d'être effectué, il faudra faire apparaitre
+//un indicateur de chargement dont le html est fourni dans le code
+//Enfin, il faudra que pour chaque enregistrement que les données s'affiche dans le
+//tableau html fourni
